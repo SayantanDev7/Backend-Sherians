6265c0e623c9671f738a4cb4f6a0d384770bd14d
Sayantan699 <sayantanhalder43@gmail.com>
2026-04-15 16:56:15 +0530

    Added get and post api functionality

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,31 @@ const express = require('express');
 
 //created a server
 const app = express();
+app.use(express.json()); //express.json is the middleware which convert req.body to json.format 
 
 app.post('/note',(req,res) =>{
     console.log(req.body);
+})
+
+const notes = [];
+
+//post is the method and notes is the API name
+app.post("/notes",(req,res) =>{
+    console.log(req.body);
+    notes.push(req.body);
+
+    res.status(201).json({ //201 denotes resources added successfully
+        message:"Note added successfully",
+        note:req.body
+    })
+})
+
+app.get("/notes",(req,res) =>{
+    res.status(200).json({ //200 denotes The request was successful.
+
+        message:"Notes fetched successfully",
+        notes:notes
+    });
 })
 
 //exporting the app
