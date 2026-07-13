48fb63dd6ad51b0626c53df318e0497e85bd9239
Sayantan699 <sayantanhalder43@gmail.com>
2026-07-13 18:53:37 +0530

    Server created using express

--- ServerCreation/server.js
+++ ServerCreation/server.js
@@ -1,16 +1,42 @@
-const http = require("http")
+// const http = require("http")
 
-const server = http.createServer((req,res) =>{ //creating the server
-    if(req.url === "/"){
-        res.end("Hello World");
-    }
-    if(req.url === "/notes"){
-        res.end("Notes API");
-    }
-    if(req.url === "/users"){
-        res.end("Users API");
-    }
+// const server = http.createServer((req,res) =>{ //creating the server
+//     if(req.url === "/"){
+//         res.end("Hello World");
+//     }
+//     if(req.url === "/notes"){
+//         res.end("Notes API");
+//     }
+//     if(req.url === "/users"){
+//         res.end("Users API");
+//     }
+// });
+// server.listen(3000,() =>{ //starting the server
+//     console.log("Server started on port 3000"); //logging the message to the console
+// });
+
+//We done use http for creating server because it is very time consuming and there are many things to do
+//so we use express for creating server
+
+
+const express = require('express');
+
+const app = express(); //calling express() is as same as createServer()
+
+app.get("/",(req,res)=>{
+    res.end("Hello World by express");
+})
+
+app.get("/notes",(req,res)=>{
+    res.end("Notes API by express");
+})
+
+app.get("/users",(req,res)=>{
+    res.end("Users API by express");
+})
+
+app.listen(3000,() =>{
+    console.log("Server started on port 3000");
 });
-server.listen(3000,() =>{ //starting the server
-    console.log("Server started on port 3000"); //logging the message to the console
-});
+
+
