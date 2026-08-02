e1e957c3f756b3dec496f79d41c4612f7e06074a
Sayantan699 <sayantanhalder43@gmail.com>
2026-08-03 01:10:12 +0530

    Start server only if my nodejs application is succesfully connected to db

--- Moody-Player/Backend/server.js
+++ Moody-Player/Backend/server.js
@@ -2,7 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./src/app.js";
 import connectToDb from "./config/db.js";   // default import — no curly braces
-connectToDb();
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+
+const startServer = async () => {
+    try {
+        await connectToDb(); //promise we await here because this is async function and it returns a promise 
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
